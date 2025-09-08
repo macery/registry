94b18ee1c972b6fba5e2aec703e6e7319b7407b9
Adam McCurry <adam.mccurry@caci.com>
2025-09-08 12:37:59 -0400

    reset the argv counts for all the commands

--- regtool.cpp
+++ regtool.cpp
@@ -53,18 +53,18 @@ int main(int argc, char* argv[]) {
             std::cout << val << "\n";
         }
         else if (command == "set-dword") {
-            if (argc != 4) throw std::runtime_error("Usage: set-dword <RegistryKeyPath> <ValueName> <Value>");
+            if (argc != 5) throw std::runtime_error("Usage: set-dword <RegistryKeyPath> <ValueName> <Value>");
             RegistryKey key(root, keyPath, true);   // set: create if missing
             DWORD val = std::strtoul(argv[4], nullptr, 0);
             key.setDword(argv[3], val);
         }
         else if (command == "get-string") {
-            if (argc != 3) throw std::runtime_error("Usage: get-string <RegistryKeyPath> <ValueName>");
+            if (argc != 4) throw std::runtime_error("Usage: get-string <RegistryKeyPath> <ValueName>");
             RegistryKey key(root, keyPath, false);
             std::cout << key.getString(argv[3]) << "\n";
         }
         else if (command == "set-string") {
-            if (argc != 4) throw std::runtime_error("Usage: set-string <RegistryKeyPath> <ValueName> <Value>");
+            if (argc != 5) throw std::runtime_error("Usage: set-string <RegistryKeyPath> <ValueName> <Value>");
             RegistryKey key(root, keyPath, true);
             key.setString(argv[3], argv[4]);
         }
